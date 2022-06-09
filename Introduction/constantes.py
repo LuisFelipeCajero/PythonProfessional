024b4aecd74bc318891bc142114b5920c884ea21
Luis Felipe Cajero <99145300+LuisFelipeCajero@users.noreply.github.com>
2022-06-08 22:37:20 -0500

    Create constantes.py

---
+++ Introduction/constantes.py
@@ -0,0 +1,5 @@
+#No existen las constantes en python, pero se llego a un consenso donde las constantes seran declaradas con letras mayúsculas.
+
+MARCA_COMPUTADORA = 'Macbook Pro'
+
+print(MARCA_COMPUTADORA)

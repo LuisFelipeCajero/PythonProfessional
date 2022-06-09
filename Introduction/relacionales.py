18229f5c0e1de3ef952d5516d26d40ef679af4fc
Luis Felipe Cajero <99145300+LuisFelipeCajero@users.noreply.github.com>
2022-06-08 22:39:57 -0500

    Create relacionales.py

---
+++ Introduction/relacionales.py
@@ -0,0 +1,17 @@
+#Operadores relacionales
+
+numero_uno = 10
+numero_dos = 15
+
+resultado = numero_uno < numero_dos
+print(resultado)
+resultado = numero_uno > numero_dos
+print(resultado)
+resultado = numero_uno <= numero_dos
+print(resultado)
+resultado = numero_uno >= numero_dos
+print(resultado)
+resultado = numero_uno == numero_dos
+print(resultado)
+resultado = numero_uno != numero_dos
+print(resultado)
